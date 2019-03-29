ade40450f7725d508ce91ab9bd514cac521ee9a5
7_5P <76pollet@gmail.com>
2019-03-29 10:36:13 +0300

    conditional rendering in vue

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ var app = new Vue({
         product: "Socks",
         description: "Footwear",
         image: "images/sock.jpeg",
-        link: "https://www.vuemastery.com/courses/intro-to-vue-js/attribute-binding"
+        link: "https://www.vuemastery.com/courses/intro-to-vue-js/attribute-binding",
+        inventory: 10,
+        onSale: true
     }
 })
