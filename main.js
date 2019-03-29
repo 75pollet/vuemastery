2c8a508751378cf7bb7d0be0ccfeb9db571cfb9c
7_5P <76pollet@gmail.com>
2019-03-29 10:21:45 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+    el: "#app",
+    data: {
+        product: "Socks",
+        description: "Footwear",
+        image: "images/sock.jpeg",
+        link: "https://www.vuemastery.com/courses/intro-to-vue-js/attribute-binding"
+    }
+})
