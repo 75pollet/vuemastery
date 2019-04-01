2d42775cd475a2cc0d8c340928dedf2c44b927a0
7_5P <76pollet@gmail.com>
2019-04-01 10:23:22 +0300

    event handling

--- main.js
+++ main.js
@@ -11,13 +11,28 @@ var app = new Vue({
         variants: [
             {
                 variantId: 1234,
-                variantColor: "Pink"
+                variantColor: "Pink",
+                variantImage: "images/sock.jpeg"
+
             },
             {
                 variantId: 3456,
-                variantColor: "Purple"
+                variantColor: "Green",
+                variantImage: "images/sock2.jpeg"
             }
         ],
-        sizes: ["Small - S", "Medium - M", "Large - L", "Extra Large - XL"]
+        sizes: ["Small - S", "Medium - M", "Large - L", "Extra Large - XL"],
+        cart: 0
+    },
+    methods: {
+        addToCart(){
+            this.cart += 1
+        },
+        updateProduct(variantImage){
+            this.image = variantImage
+        },
+        reduceNumber(){
+            this.cart -= 1
+        }
     }
 })
