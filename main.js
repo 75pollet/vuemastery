ed7eedadd9494c574c5c4b7648822b728a4f3899
7_5P <76pollet@gmail.com>
2019-04-01 09:53:57 +0300

    list Rendering

--- main.js
+++ main.js
@@ -6,6 +6,18 @@ var app = new Vue({
         image: "images/sock.jpeg",
         link: "https://www.vuemastery.com/courses/intro-to-vue-js/attribute-binding",
         inventory: 10,
-        onSale: true
+        onSale: true,
+        details: ["20% cotton", "30% polyester", "100% awesome"],
+        variants: [
+            {
+                variantId: 1234,
+                variantColor: "Pink"
+            },
+            {
+                variantId: 3456,
+                variantColor: "Purple"
+            }
+        ],
+        sizes: ["Small - S", "Medium - M", "Large - L", "Extra Large - XL"]
     }
 })
